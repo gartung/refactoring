0a9c0528b2b40339d93dfc08cc8545c00e5dea6d
Patrick Gartung <gartung@fnal.gov>
2016-03-03 17:52:57 -0600

    follow example of update_for_art.sh

--- fix_cmake_nutools.pl
+++ fix_cmake_nutools.pl
@@ -3,18 +3,6 @@ use strict;
 use vars qw(%dir_list);
 BEGIN { %dir_list = (
 	"SimulationBase" => "nusimdata_SimulationBase",
-	"SimulationBase_dict" => "nusimdata_SimulationBase_dict",
-	"EventDisplayBase" => "nutools_EventDisplayBase",
-	"EventDisplay" => "nutools_EventDisplay",
-	"EventGeneratorBaseCRY" => "nutools_EventGeneratorBaseCRY",
-	"EventGeneratorBaseGENIE" => "nutools_EventGeneratorBaseGENIE",
-	"EventGeneratorBase_test_EventGeneratorTest_module" => "nutools_EventGeneratorBase_test_EventGeneratorTest_module",
-	"IFDatabase" => "nutools_IFDatabase",
-	"G4Base" => "nutools_G4Base",
-	"NuBeamWeights" => "nutools_NuBeamWeights",
-	"NuReweightArt" => "nutools_NuReweightArt",
-	"NuReweight" => "nutools_NuReweight",
-	"MagneticField_service" => "nutools_MagneticField_MagneticField_service"
 		       ); }
 
 foreach my $lib (sort keys %dir_list) {
@@ -23,3 +11,5 @@ foreach my $lib (sort keys %dir_list) {
   #s&\b\Q${lib}\E([^\.\s]*\b)([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
   s&\b\Q${lib}\E\b([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
 }
+
+s&\$\{SIMULATIONBASE\}&nusimdata_SimulationBase&g;
